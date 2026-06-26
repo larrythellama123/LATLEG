3291746487b9dbc81e715446fefc0f67af877d1e
Rohit <rkurup01@gmail.com>
2026-06-27 02:53:39 +0800

    new4

--- src/client.cpp
+++ src/client.cpp
@@ -9,7 +9,7 @@
 #include "player.hpp"
 #include <csignal>
 #include <atomic>
-
+#include <fcntl.h>
 
 #define PORT     8080
 #define MAXLINE  1024
@@ -21,59 +21,67 @@ void signal_handler(int signum) {
     }
 }
 
-int main() {
-    std::signal(SIGINT, signal_handler);
-    std::unique_ptr<Player> player = std::make_unique<Player>(); 
-    int sockfd;
-    char buffer[MAXLINE];
-    const char *hello = "Hello from client";
-    struct sockaddr_in servaddr, cliaddr;
+    int main() {
+        std::signal(SIGINT, signal_handler);
+        std::unique_ptr<Player> player = std::make_unique<Player>(); 
+        int sockfd;
+        char buffer[MAXLINE];
+        const char *hello = "Hello from client";
+        struct sockaddr_in servaddr, cliaddr;
 
-    // Create UDP socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
-
-
-    // Fill server address info
-    servaddr.sin_family = AF_INET;              // IPv4
-    servaddr.sin_port   = htons(PORT);          // Server port
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server IP
-
-    socklen_t len = sizeof(servaddr);
-    PlayerPacketOutput received_packet;
-    while(keep_running.load()){
-        //changes to the player location then send to server
-        player->processInput();
-        PlayerPacketInput payload= player->formPacket();
-        sendto(sockfd, reinterpret_cast<const char *> (&payload), sizeof(payload), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-        ssize_t bytes_received = recvfrom(
-            sockfd, 
-            &received_packet,         // Pointer to your struct
-            sizeof(received_packet),  // Expected size in bytes
-            0, 
-            (struct sockaddr*)&cliaddr, 
-            &len
-        );
-
-        if(bytes_received ==  sizeof(received_packet)){
-            render(received_packet);
+        // Create UDP socket
+        sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        if (sockfd < 0) {
+            perror("socket creation failed");
+            exit(EXIT_FAILURE);
         }
-        else{
-            std::cerr<<"there was an error";
+        fcntl(sockfd, F_SETFL, O_NONBLOCK);
+
+        memset(&servaddr, 0, sizeof(servaddr));
+        memset(&cliaddr, 0, sizeof(cliaddr));
+
+
+        // Fill server address info
+        servaddr.sin_family = AF_INET;              // IPv4
+        servaddr.sin_port   = htons(PORT);          // Server port
+        servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server IP
+
+        socklen_t len = sizeof(servaddr);
+        PlayerPacketOutput received_packet;
+        while(keep_running.load()){
+            //changes to the player location then send to server
+            player->processInput();
+            PlayerPacketInput payload= player->formPacket();
+            sendto(sockfd, reinterpret_cast<const char *> (&payload), sizeof(payload), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+            ssize_t bytes_received = recvfrom(
+                sockfd, 
+                &received_packet,         
+                sizeof(received_packet),  
+                0, 
+                (struct sockaddr*)&cliaddr, 
+                &len
+            );
+
+            if(bytes_received ==  sizeof(received_packet)){
+                render(received_packet);
+            }
+            else if (bytes_received < 0) {
+                if (errno == EWOULDBLOCK || errno == EAGAIN) {
+
+                } else {
+                    std::cerr << "Actual socket read error occurred\n";
+                }
+            }
+            else{
+                std::cerr<<"there was an error";
+            }
+            
         }
-        
+
+    
+
+        // Close socket
+        close(sockfd);
+
+        return 0;
     }
-
-  
-
-    // Close socket
-    close(sockfd);
-
-    return 0;
-}
