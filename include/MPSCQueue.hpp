82ea9c599fc206a05d1df71e28a9be1508462120
RohitK <rkurup01@gmail.com>
2026-06-30 23:28:13 +0800

    fix bugs

--- include/MPSCQueue.hpp
+++ include/MPSCQueue.hpp
@@ -1,63 +1,34 @@
 #include <atomic>
+#include <thread>
+#include <chrono>
 
 template<typename T, uint32_t size>
 class MPSCQueue{
     static_assert(size && !(size & (size - 1)), "size must be a power of 2");
-    struct Reader{
-        T* read(){
-            auto& blk = q->blks[next_idx % size];
-            uint32_t new_idx = blk.idx.load(std::memory_order_acquire);
-            if(int(new_idx - next_idx) < 0) return nullptr;
-            next_idx = new_idx + 1;
-            return &blk.data;
-        }
-
-        T* readLast(){
-            T* ret = nullptr;
-            while(T* cur = read()){
-                ret=cur;
-            }   
-            return ret;
-        }
-
-        
-
-        MPSCQueue<T, size>* q = nullptr;
-        uint32_t next_idx;
-    }
-
-    struct Writer{
-        T* write(){
-            auto& blk = blks[++write_idx % size];
-            writer(blk.data); 
-            blk.idx.store(write_idx, std::memory_order_release);
-            auto& blk = q->blks[next_idx % size];
-            uint32_t new_idx = blk.idx.load(std::memory_order_acquire);
-            if(int(new_idx - next_idx) < 0) return nullptr;
-            next_idx = new_idx + 1;
-            return &blk.data;
-        }
-
-        
-        
-        MPSCQueue<T, size>* q = nullptr;
-        uint32_t next_idx;
-    }
-
+    
     template<typename Writer>
-    write(Writer writer){
-        auto& blk = blks[++write_idx % size];
-        writer(blk.data); 
-        blk.idx.store(write_idx, std::memory_order_release);   
+    struct Writer{
+        bool write(){
+            while(true){
+                uint32_t write_idx_local = write_idx.load(std::memory_order_relaxed);
+                if(write_idx.compare_exchange_weak(write_idx_local, write_idx_local+1,std::memory_order_acquire)){
+                    continue;
+                }
+                write_idx_local = write_idx.load(std::memory_order_acquire);
+                auto& blk = blks[write_idx.store((write_idx_local+1)%size, std::memory_order_release)];
+                writer(blk.data); 
+                blk.ready.store(true, std::memory_order_release);
+            }
+        }
+        
+        MPSCQueue<T, size>* q = nullptr;
+        struct alignas(64) Block
+        {
+            std::atomic<bool> ready = false;
+            T data;
+        }blks[size];
+
+        alignas(128) std::atomic<uint32_t> write_idx = 0; 
     }
 
-    private:
-    friend class Reader;
-    struct alignas(64) Block
-    {
-        std::atomic<uint32_t> idx = 0;
-        T data;
-    }blks[size];
-
-    alignas(128) uint32_t write_idx = 0; 
 }
